eaf9f3f0d9feccc952ac7370acf6197471238417
iraritchiemeek <iraritchiemeek@gmail.com>
2015-03-26 12:39:19 +1300

    Got snakes working to an extent, need to fix error of snakes going off page

--- snakes_and_ladders.rb
+++ snakes_and_ladders.rb
@@ -10,18 +10,35 @@ require 'pp'
 class SnakesAndLadders
 
   def initialize
-
+    @start_row = start_row
+    @start_column = start_column
+    @snake_length = snake_length
   end
 
   def empty_board
     @board = Array.new(10) { Array.new(10) { ' ' } }
   end
 
-  def snake
-    @board[-1][rand(0..10)] = 'S'
-    pp @board
+  def start_row
+    @start_row = rand(0..8)
   end
 
+  def start_column
+    @start_column = rand(0..8)
+  end
+
+  def snake_length
+    @snake_length = rand(1..6)
+  end
+
+  def snake
+    puts "Start row: #{@start_row}, Start column #{@start_column}, Snake Length #{@snake_length}"
+    @board[@start_row.to_i][@start_column.to_i] = 'S'
+      @snake_length.to_i.times do
+        @board[@start_row+=1][@start_column+=1] = 's'
+      end
+    pp @board
+  end
 end
 
 new_board = SnakesAndLadders.new
